9288589fa72ba42e55d447f4ac8050affd0b9542
Josue Galvez <156706604+galvezjosue19@users.noreply.github.com>
2024-03-07 23:00:16 -0500

    updates to files

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,19 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded' , (event) =>{
     getVisitCount();
 })
 
-const productionApiUrl = 'https://azureresumevisitorcounter.azurewebsites.net/api/GetVisitorCounter?';
-const localApiUrl = 'http://localhost:7071/api/GetVisitorCounter';
-
+const functionApiURL = "https://counterfunc.azurewebsites.net/api/HttpTriggerCounter2"
+const functionApiLocal  = 'http://localhost:7071/api/HttpTriggerCounter2';
 const getVisitCount = () => {
     let count = 30;
-    fetch(productionApiUrl).then(response => {
+    fetch(functionApiURL).then(response => {
         return response.json()
+        //return response
     }).then(response =>{
         console.log("Website called function API.");
-        count =  response.count;
+        console.log(response);
+        //count = response.count;
+        count = response
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
